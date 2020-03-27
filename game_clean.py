6feca5780f2fbf83a5f3bd243bb2718ff2c39108
Matt Weston <45575140+RealMizat@users.noreply.github.com>
2020-03-26 23:51:33 -0400

    This is a third update not any major changes
    This version has walking in all 4 directions, as well as animations for those directions (witch will be replaced, trust me it's temporary and all I could find complete. Ignore the tiny sailor moon sprite on the large Zelda/Pokemon/Kirby mashup forest.). I will be doing something with the jump, as this will be top down, and it will not act how it does. Notice when you hold up or down and then click left or right, it is weird and needs to be patched up. See how when you hold left or right you cannot press another direction, that is how each direction should be, I think. I'm gonna play a few good RPGs and see what the movement is like can't remember cause it's something you just do. I may implement another 4 directions, NW, NE, SW, SE. When holding 2 directions, and will have to alter the base code for that. Next upload of this will be big changes to the entire format and way it is programed. I will make sprites when I get time. I usually would have tons of little things to upload, like Tkinter random windows with some functionality, but I have been using my time on this, and researching game development, also some tkinter stuff, but haven't made much of anything important, if I have it was from a guide.

--- game_clean.py
+++ game_clean.py
@@ -17,9 +17,11 @@ walk_right = [pygame.image.load('right1.png'),pygame.image.load('right2.png'),py
 walk_left = [pygame.image.load('left1.png'),pygame.image.load('left2.png'),pygame.image.load('left3.png'),pygame.image.load('left4.png'),
              pygame.image.load('left5.png')]
 
-walk_forward = [pygame.image.load('forward1.png'), pygame.image.load('forward2.png'), pygame.image.load('forward3.png')]
+walk_backward = [pygame.image.load('forward1.png'), pygame.image.load('forward2.png'), pygame.image.load('forward3.png'),pygame.image.load('forward1.png'),
+                pygame.image.load('forward2.png'), pygame.image.load('forward3.png')]
 
-walk_backward = [pygame.image.load('backward1.png'), pygame.image.load('backward2.png'), pygame.image.load('backward3.png')]
+walk_forward = [pygame.image.load('backward1.png'), pygame.image.load('backward2.png'), pygame.image.load('backward3.png'),pygame.image.load('backward1.png'),
+                 pygame.image.load('backward2.png'), pygame.image.load('backward3.png')]
 
 bg = pygame.image.load('backround.png')
 
@@ -33,14 +35,16 @@ char = pygame.image.load('stand1.png')
 clock = pygame.time.Clock()
 
 x = 60
-y = 35
-width = 24
-height = 36
+y = 640
+width = 12
+height = 32
 vel = 6
 is_jump = False
-jump_count = 10
+jump_count = 8
 left = False
 right = False
+up = False
+down = False
 walk_count = 0
 
 
@@ -58,6 +62,12 @@ def redrawGameWindow():
         
     elif right:
         win.blit(walk_right[walk_count//3], (x, y))
+        walk_count += 1
+    elif up:
+        win.blit(walk_forward[walk_count//3], (x, y))
+        walk_count += 1
+    elif down:
+        win.blit(walk_backward[walk_count//3], (x, y))
         walk_count += 1
     else:
         win.blit(char, (x, y))
@@ -85,10 +95,18 @@ while run:
         x -= vel
         left = True
         right = False
-    elif keys[pygame.K_RIGHT] and x < 700 - width - vel: #  X < Screen Width - Width - vel
+    elif keys[pygame.K_RIGHT] and x < 800 - width - vel: #  X < Screen Width - Width - vel
         x += vel
         right = True
         left = False
+    elif keys[pygame.K_UP] and y > vel:
+        y -=vel
+        up = True
+        down = False
+    elif keys[pygame.K_DOWN] and y < 800 - height - vel :
+        y += vel
+        down = True
+        up = False
     else:
         right = False
         left = False
@@ -106,7 +124,7 @@ while run:
 
         
     else:
-        if jump_count >= -10:
+        if jump_count >= -8:
             neg = 1
                                 # positive neg here to keep -10 to increase y axis (thats backwords)
             if jump_count < 0:
@@ -115,7 +133,7 @@ while run:
             jump_count -= 1
         else:
             is_jump = False
-            jump_count = 10
+            jump_count = 8
            
          
     redrawGameWindow()
