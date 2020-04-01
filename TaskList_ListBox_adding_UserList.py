c8d11b066c4eef136627d24722f238751f7ee591
Matt Weston <45575140+RealMizat@users.noreply.github.com>
2020-03-31 20:52:26 -0400

    Found on youtube comment and fixed it, random.
    Watching a youtube video looking at ways to display user created lists in tkinter, (this video https://www.youtube.com/watch?v=QcXcspHRI3Q). I found some code in the comments or a link, asking why it didn't work, so I fixed it real quick, and that saves me from quickly working on that game, or a Tkinter GUI rushing to get it done to post it here.

---
+++ TaskList_ListBox_adding_UserList.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+import time
+ 
+presentDate =  time.asctime()[0:11]
+ 
+root=tk.Tk()
+root.configure(bg="black")
+root.geometry("300x200")
+tasks = []
+tasks=["1", "2", "3"]
+ 
+ 
+def update_listbox():
+    for task in tasks:
+        lb_update.insert("end", task)
+ 
+def addTask():
+    update_listbox()
+ 
+def completeTask():
+    pass
+ 
+date=tk.Label(text=presentDate)
+date.pack()
+title = tk.Label(root, text="Welcome Name",)
+title.pack()
+entry = tk.Entry(root)
+entry.pack()
+add_task = tk.Button(root, text="Add Task", command=update_listbox)
+add_task.pack()
+completeTask = tk.Button(root, text="Finish Task", command=completeTask())
+completeTask.pack()
+lb_update = tk.Listbox(root)
+lb_update.pack()
+
+root.mainloop()
