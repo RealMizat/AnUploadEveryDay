2eb74340d34553e62fc025223fa93b88bab76f43
Matt Weston <45575140+RealMizat@users.noreply.github.com>
2020-05-10 16:08:15 -0400

    Starter Level Simple Mothers Day If/Else Python
    Could be converted to while loop rather easily, however I went very basic this year, it's the thought that counts.

---
+++ Mom_else_if.py
@@ -0,0 +1,9 @@
+#while True:
+prompt = input(str('Enter the Date: MM/DD/Year : \n Press Enter  \n'))
+
+if prompt == str('05/10/2020'):
+       print('Happy Mothers Day! Love Matt')
+elif prompt == str('10/05/2020'):
+       print('Learn to follow instructions. Happy Mothers Day.')
+else:
+       print('Wow you must have failed really bad. Happy Mothers Day and Try Again')
